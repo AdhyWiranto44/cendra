d717d44c4a7f0ae8eecaa3c3b2c389c1ec7b69c7
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-08-10 17:47:26 +0700

    feat: use html script

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -343,6 +344,49 @@ export default function Home() {
         </div>
       </div>
       {/* <!-- End of Explore Modal --> */}
+
+      <Script id='myscript'>
+        {`const tweetModal = document.getElementById('tweet-modal');`}
+        {`const tweetModalExit = document.getElementById('tweet-modal-exit');`}
+        {`const tweetSidebarButton = document.getElementById('tweet-sidebar-button');`}
+        {`const tweetInput = document.getElementById('tweet-component').getElementsByTagName('textarea')[0];`}
+        {`const tweetInputFloated = tweetModal.getElementsByTagName('textarea')[0];`}
+
+        {`const exploreModal = document.getElementById('explore-modal');`}
+        {`const exploreButton = document.getElementById('explore');`}
+        {`const exploreModalExit = document.getElementById('explore-modal-exit');`}
+
+        {`const searchButton = document.getElementById('search-button');`}
+
+        {`tweetSidebarButton.addEventListener('click', () => {
+          tweetModal.classList.remove('hidden');
+        });`}
+
+        {`tweetModalExit.addEventListener('click', () => {
+          tweetModal.classList.add('hidden');
+        });`}
+
+        {`tweetInput.addEventListener('focus', () => {
+          tweetInput.classList.add('border-b-2');
+        });`}
+
+        {`tweetInputFloated.addEventListener('focus', () => {
+          tweetInputFloated.classList.add('border-b-2');
+        });`}
+
+        {`exploreButton.addEventListener('click', () => {
+          if (window.innerWidth < 1024) {
+          exploreModal.classList.remove('hidden');
+          } else {
+          searchButton.focus();
+          }
+        });`}
+
+        {`exploreModalExit.addEventListener('click', () => {
+          exploreModal.classList.add('hidden');
+        });`}
+
+      </Script>
     </div>
   )
 }
